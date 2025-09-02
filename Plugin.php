7749018fb0a291df008fbca8376607a17720bd07
israelmelo <israelg.8@gmail.com>
2025-09-02 13:09:30 -0300

    Criação do plugin.php

---
+++ Plugin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CommitteeDraw;
+
+use MapasCulturais\App;
+
+class Plugin extends \MapasCulturais\Plugin
+{
+    protected static $instance;
+
+    function __construct(array $config = [])
+    {
+        $config += [];
+
+        parent::__construct($config);
+
+        self::$instance = $this;
+    }
+
+    /**
+     * @return void
+     */
+    function register()
+    {
+        $app = App::i();
+    }
+
+    function _init() 
+    {
+        $app = App::i();
+        $self = $this;
+    }
+
+    static function getInstance()
+    {
+        return self::$instance;
+    }
+}
