8889d7736502d56a453c0f023758c745bea6b30f
israelmelo <israelg.8@gmail.com>
2025-09-05 11:14:29 -0300

    Ajustes na entidade CommitteeDraw

--- Entities/CommitteeDraw.php
+++ Entities/CommitteeDraw.php
@@ -2,10 +2,12 @@
 
 namespace CommitteeDraw\Entities;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use MapasCulturais\Entities\User;
 use MapasCulturais\Entities\File;
 use MapasCulturais\Entities\EvaluationMethodConfiguration;
+use MapasCulturais\Entity;
 
 /**
  * @ORM\Table(name="committee_draw")
@@ -14,20 +16,21 @@ use MapasCulturais\Entities\EvaluationMethodConfiguration;
 class CommitteeDraw extends \MapasCulturais\Entity
 {
     /**
-     * @var int
+     * @var integer
      *
-     * @ORM\Column(name="id", type="integer")
+     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="committee_draw_id_seq", allocationSize=1, initialValue=1)
      */
-    protected $id;
+    public $id;
 
     /**
      * @var \DateTime
      *
      * @ORM\Column(name="create_timestamp", type="datetime")
      */
-    protected $createTimestamp;
+    protected DateTime $createTimestamp;
 
     /**
      * @var User
@@ -35,7 +38,7 @@ class CommitteeDraw extends \MapasCulturais\Entity
      * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\User", fetch="LAZY")
      * @ORM\JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE", nullable=false)
      */
-    protected $user;
+    protected User $user;
 
     /**
      * @var EvaluationMethodConfiguration
@@ -43,35 +46,35 @@ class CommitteeDraw extends \MapasCulturais\Entity
      * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\EvaluationMethodConfiguration", fetch="LAZY")
      * @ORM\JoinColumn(name="evaluation_method_configuration_id", referencedColumnName="id", onDelete="CASCADE", nullable=false)
      */
-    protected $evaluationMethodConfiguration;
+    protected EvaluationMethodConfiguration $evaluationMethodConfiguration;
 
     /**
      * @var string
      *
      * @ORM\Column(name="committee_name", type="string")
      */
-    protected $committeeName;
+    protected string $committeeName;
 
     /**
      * @var int
      *
      * @ORM\Column(name="draw_number", type="integer")
      */
-    protected $drawNumber;
+    protected int $drawNumber;
 
     /**
      * @var string
      *
      * @ORM\Column(name="seed", type="string")
      */
-    protected $seed;
+    protected string $seed;
 
     /**
      * @var string
      *
      * @ORM\Column(name="file_md5", type="string", length=32)
      */
-    protected $fileMd5;
+    protected string $fileMd5;
 
     /**
      * @var File
@@ -79,26 +82,51 @@ class CommitteeDraw extends \MapasCulturais\Entity
      * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\File", fetch="LAZY")
      * @ORM\JoinColumn(name="file_id", referencedColumnName="id", onDelete="SET NULL", nullable=true)
      */
-    protected $file;
+    protected File $file;
 
     /**
      * @var int
      *
      * @ORM\Column(name="number_of_valuers", type="integer")
      */
-    protected $numberOfValuers;
+    protected int $numberOfValuers;
 
     /**
      * @var array
      *
      * @ORM\Column(name="input_valuers", type="json")
      */
-    protected $inputValuers = [];
+    protected array $inputValuers = [];
 
     /**
      * @var array
      *
      * @ORM\Column(name="output_valuers", type="json")
      */
-    protected $outputValuers = [];
+    protected array $outputValuers = [];
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="smallint",  nullable=false)
+     */
+    protected int $status = Entity::STATUS_ENABLED;
+
+    public static function getControllerId()
+    {
+        return 'committeedraw';
+    }
+
+    protected function canUserCreate($user){
+        return $this->evaluationMethodConfiguration->canUser('create', $user);
+    }
+
+    protected function canUserModify($user){
+        return $this->evaluationMethodConfiguration->canUser('modify', $user);
+    }
+
+    protected function canUserRemove($user){
+        return $this->evaluationMethodConfiguration->canUser('remove', $user);
+    }
+
 }
