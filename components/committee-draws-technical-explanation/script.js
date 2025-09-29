67888bef9cebfa1a94d97c7dd9ecc91797329832
Welington De Lima Olegario <wolegario49@gmail.com>
2025-09-29 10:13:57 -0300

    Criar o componente committee-draws-info para exibir as informações

---
+++ components/committee-draws-technical-explanation/script.js
@@ -0,0 +1,15 @@
+app.component('committee-draws-technical-explanation', {
+    template: $TEMPLATES['committee-draws-technical-explanation'],
+
+    props: {
+    },
+
+    computed: {
+
+    },
+
+    data() {
+        return {
+        }
+    },
+});
